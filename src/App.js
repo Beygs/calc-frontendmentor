a909ddad2db56fd1e1e4ed894ecbaae3615558e4
Boris Gilles <boris.gilles@hotmail.fr>
2021-05-28 02:12:23 +0200

    attribution

--- src/App.js
+++ src/App.js
@@ -51,7 +51,12 @@ function App() {
           />
         </div>
         <div className="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Challenge by <a 
+              href="https://www.frontendmentor.io?ref=challenge" 
+              target="_blank"
+              rel="noreferrer">
+              Frontend Mentor
+            </a>. 
           Coded by <a href="https://github.com/Beygs">Beygs</a>.
         </div>
       </div>
