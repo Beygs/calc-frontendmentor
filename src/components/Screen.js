5278accc755c4dfab9bc510610c795590ad805bc
Boris Gilles <boris.gilles@hotmail.fr>
2021-05-28 14:53:51 +0200

    Accessibility issues fixed + Updated screen display

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,17 +3,19 @@ import React from "react";
 const Screen = ({ result, resultSize, memory, operator }) => {
     return (
         <div className="screen">
-            <div className="outer-result">
+            <div className="inner-screen">
                 <h3 
                     className="memory" 
                     style={{display: `${memory === 0 ? "none" : "block"}`}}>
                         {memory} {operator}
                 </h3>
-                <h2 
-                    className="result" 
-                    style={{fontSize: `${resultSize}em`, height: `${resultSize}em`}}>
-                        {result}
-                </h2>
+                <div className="outer-result">
+                    <h2 
+                        className="result" 
+                        style={{fontSize: `${resultSize}em`}}>
+                            {result}
+                    </h2>
+                </div>
             </div>
         </div>
     )
